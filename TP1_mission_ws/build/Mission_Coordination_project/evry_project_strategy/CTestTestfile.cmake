275e8e76dcdc94357020b4b103bac0130f727ea9
Efe Şendil <efesendil@pc-9-234.customer.ask4.lan>
2023-01-08 16:45:18 +0100

    Update

--- TP1_mission_ws/build/Mission_Coordination_project/evry_project_strategy/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/efesendil/TP1_mission_ws/src/Mission_Coordination_project/evry_project_strategy
-# Build directory: /home/efesendil/TP1_mission_ws/build/Mission_Coordination_project/evry_project_strategy
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
