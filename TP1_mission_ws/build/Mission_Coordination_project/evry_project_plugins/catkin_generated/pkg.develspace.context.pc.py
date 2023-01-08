53ddf2a59c0edebc78e45371bd6718f394d0252b
Efe Şendil <efesendil@pc-9-234.customer.ask4.lan>
2023-01-08 16:39:58 +0100

    Update

---
+++ TP1_mission_ws/build/Mission_Coordination_project/evry_project_plugins/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/efesendil/TP1_mission_ws/devel/include".split(';') if "/home/efesendil/TP1_mission_ws/devel/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "evry_project_plugins"
+PROJECT_SPACE_DIR = "/home/efesendil/TP1_mission_ws/devel"
+PROJECT_VERSION = "0.0.0"
